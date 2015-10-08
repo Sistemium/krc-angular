484e311d6f53590bfc9c2be44ce07471a0c42886
Denis Mosin <deniss.mosin@gmail.com>
2015-10-08 18:24:14 +0300

    400 error handler

--- server/api/krc/index.js
+++ server/api/krc/index.js
@@ -7,7 +7,8 @@ var router = express.Router();
 
 router.get('/:word', controller.index);
 router.get('/', function(req, res){
-
+  res.status(400).end('Wrong input');
+  console.log('Wrong input');
 });
 
 
