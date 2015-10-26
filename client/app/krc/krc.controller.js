57aa932d22b838756c7d655eef36f495fb6bf453
Denis Mosin <deniss.mosin@gmail.com>
2015-10-26 13:45:42 +0200

    Toast, left menu controllers added, writing a searched word controller added

--- client/app/krc/krc.controller.js
+++ client/app/krc/krc.controller.js
@@ -1,32 +1,68 @@
 'use strict';
 
 angular.module('stklcApp')
-  .controller('KrcCtrl', function ($http) {
+  .controller('KrcCtrl', function ($http, $scope, $mdToast, $mdSidenav) {
+
+    var toastPosition = {
+      bottom: false,
+      top: true,
+      left: false,
+      right: true
+    };
 
     var me = this;
-    var notfound = false;
-    angular.extend (me, {
 
+    angular.extend (me, {
+      history: [],
       kirciuoti: function (word) {
 
         var w = word || me.wordInput;
-
         $http.get('/api/krc/' + w).success(function(data) {
+          me.writeSearchedWords(w);
           me.data = data;
-          me.errMsg ='';
         }).error(function(){
           me.data = [];
-          me.notfound = true;
+          me.showSimpleToast();
         });
 
       },
 
-      wordInput: ''
+      writeSearchedWords: function(searchedWord){
+        if(me.history.length < 10){
+          me.history.push({word: searchedWord});
+        }
+        else{
+          me.history.shift();
+          me.history.push({word: searchedWord});
+        }
+      },
+
+      showSimpleToast: function(){
+        $mdToast.show(
+          $mdToast.simple()
+            .content('Žodis nerastas!')
+            .position(me.getToastPosition())
+            .hideDelay(2000)
+        );
+      },
+
+      getToastPosition: function() {
+        return Object.keys(toastPosition)
+          .filter(function(pos) { return toastPosition[pos]; })
+          .join(' ');
+      },
+
+      toggleLeft:function(){
+        $mdSidenav('left-nav').toggle();
+      },
+
+      closeSideNav: function() {
+        $mdSidenav('left-nav').close();
+      },
+
+      wordInput: 'Mama',
 
     });
-
-
-
 
   });
 
