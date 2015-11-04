40b97569f1fca82d3d7c84ff9cfa529c294909ac
Alexander Levin <alevin.ru@gmail.com>
2015-11-05 00:02:39 +0200

    main.controller.js

--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -1,17 +1,29 @@
 'use strict';
 
 angular.module('stklcApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ['$scope', function ($scope) {
 
     var me = this;
 
     angular.extend(me,{
+
       navs: [
-        { name: 'krc', title: 'Kirčiavimas'},
-        { name: 'about', title: 'Apie projektą'}
+        { name: 'krc', title: 'Kirčiavimas', sref: 'krc', icon: 'action:exit_to_app'},
+        { name: 'about', title: 'Apie projektą', sref: 'about', icon: 'action:question_answer'}
       ],
 
-      
+      openMenu: function($mdOpenMenu, ev) {
+        $mdOpenMenu(ev);
+      }
+
     });
 
-  });
+    $scope.setSubNavs = function(navs) {
+      me.subNavs = navs || [];
+    };
+
+    $scope.$on('$stateChangeSuccess', function() {
+      $scope.setSubNavs();
+    });
+
+  }]);
