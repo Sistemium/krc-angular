d2c436d9f4a0c2b223167f856388ca23c24f25ee
TaWReOn <deniss.mosin@gmail.com>
2015-11-20 21:49:51 +0200

    /strp route added

--- server/api/krc/index.js
+++ server/api/krc/index.js
@@ -6,11 +6,16 @@ var controller = require('./krc.controller');
 var router = express.Router();
 
 router.get('/:word', controller.index);
+
 router.get('/', function(req, res){
   res.status(400).end('Wrong input');
   console.log('Wrong input');
 });
 
+router.get('/strp', function(req, res){
+  res.status(200);
+  console.log('Hello');
+});
 
 
 module.exports = router;
