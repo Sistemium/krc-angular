ff6e6fa519709aa82c20e763516ad47fa3396bd6
TaWReOn <deniss.mosin@gmail.com>
2015-11-23 13:52:45 +0200

    watch .jade extensions

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function (grunt) {
       },
       jade: {
         files: [
-          '<%= yeoman.client %>/{app,components}/*',
+          '<%= yeoman.client %>/{app,components}/*.jade',
           '<%= yeoman.client %>/{app,components}/**/*.jade'],
         tasks: ['jade']
       },
