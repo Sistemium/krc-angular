a183c218fa9a9ac0067780d65cd2b33411e47b99
TaWReOn <deniss.mosin@gmail.com>
2015-12-02 13:05:08 +0200

    Directive for getting screen height and width added

--- client/app/krc/krc.controller.js
+++ client/app/krc/krc.controller.js
@@ -2,174 +2,199 @@
 
 angular.module('stklcApp').controller('KrcCtrl', [
     '$http', '$scope', '$mdToast', '$mdSidenav', '$window', '$uiViewScroll', '$document',
-  function ($http, $scope, $mdToast, $mdSidenav, $window, $uiViewScroll, $document) {
+    function ($http, $scope, $mdToast, $mdSidenav, $window, $uiViewScroll, $document) {
 
-    var toastPosition = {
-      bottom: false,
-      top: true,
-      left: false,
-      right: true
-    };
+      var toastPosition = {
+        bottom: false,
+        top: true,
+        left: false,
+        right: true
+      };
 
-    var me = this;
-    var localStorage = window.localStorage;
-    var w = angular.element($window);
+      var me = this;
+      var localStorage = window.localStorage;
+      var w = angular.element($window);
 
-    var resizeBind = w.bind('resize', function () {
-      if(me.isSideNavOpen){
-        me.closeSideNav();
-      }
-    });
 
-    $scope.$on('$viewContentLoaded', function() {
-
-      _.each($document.find('md-content'),function (el) {
-
-        var elem = angular.element(el);
-
-        if (elem.hasClass('scrollBind')) {
-          me.scrollTopElement = elem;
-          var scrollBind = elem.bind('scroll', function (ev) {
-            $scope.$apply(me.scrollTop = ev.target.scrollTop);
-          });
+      var resizeBind = w.bind('resize', function () {
+        if (me.isSideNavOpen) {
+          me.closeSideNav();
         }
       });
 
-    });
+      $scope.$on('$viewContentLoaded', function () {
 
-    $scope.$watchCollection('ctrl.history',function (newHistory){
-      newHistory && newHistory.length && localStorage.setItem('history',JSON.stringify(newHistory));
-    });
+        _.each($document.find('md-content'), function (el) {
 
-    $scope.$watch('ctrl.isSideNavOpen',function (nv,ov){
-      $scope.disableScroll(nv && !ov);
-    });
+          var elem = angular.element(el);
 
-    $scope.$on('$destroy', function () {
-      w.unbind('resize', resizeBind);
-    });
+          if (elem.hasClass('scrollBind')) {
+            me.scrollTopElement = elem;
+            var scrollBind = elem.bind('scroll', function (ev) {
+              $scope.$apply(me.scrollTop = ev.target.scrollTop);
+            });
+          }
+        });
 
-    angular.extend (me, {
+      });
 
-      errors: {
-        'md-maxlength': 'Žodžio ilgis per didelis',
-        'md-required': 'Žodis neįvestas'
-      },
+      $scope.$watchCollection('ctrl.history', function (newHistory) {
+        newHistory && newHistory.length && localStorage.setItem('history', JSON.stringify(newHistory));
+      });
 
-      history: JSON.parse(localStorage.getItem('history')) || [],
+      $scope.$watch('ctrl.isSideNavOpen', function (nv, ov) {
+        $scope.disableScroll(nv && !ov);
+      });
 
-      kirciuoti: function (word) {
+      $scope.$on('$destroy', function () {
+        w.unbind('resize', resizeBind);
+      });
 
-        var w = _.capitalize ((word || me.wordInput|| '').toLowerCase());
-        var errors = angular.copy($scope.wordInputForm.word.$error);
+      angular.extend (me, {
 
-        if (!w) {
+        errors: {
+          'md-maxlength': 'Žodžio ilgis per didelis',
+          'md-required': 'Žodis neįvestas'
+        },
 
-          if (!Object.keys(errors).length) {
-            errors['md-required'] = true;
+        history: JSON.parse(localStorage.getItem('history')) || [],
+
+        kirciuoti: function (word) {
+
+          var w = _.capitalize ((word || me.wordInput || '').toLowerCase());
+          var errors = angular.copy($scope.wordInputForm.word.$error);
+
+          if (!w) {
+
+            if (!Object.keys(errors).length) {
+              errors['md-required'] = true;
+            }
+
+            var msg = _.map(errors, function (val, key) {
+              return me.errors[key];
+            }).join(',');
+
+            return me.showSimpleToast(msg);
           }
 
-          var msg = _.map(errors, function(val,key){
-            return me.errors[key];
-          }).join(',');
+          $http.get('/api/krc/' + w).success(function (data) {
+            me.writeSearchedWords(w);
+            me.data = data;
+          }).error(function (data, res) {
+            if (res == 404) {
+              me.data = [];
+              me.showSimpleToast('Žodis nerastas');
+            }
+            else if (res == 400) {
+              me.showSimpleToast('Pasitikrintike įvestą žodį');
+            }
+            else if (res == 500) {
+              me.showSimpleToast('Serverio klaida');
+            }
+          });
 
-          return me.showSimpleToast(msg);
+        },
+
+        writeSearchedWords: function (searchedWord) {
+          me.duplicationRemover(searchedWord);
+          me.history.unshift(searchedWord);
+          me.history = _.take(me.history, 20);
+        },
+
+        duplicationRemover: function (wordToCheck) {
+          me.history.forEach(function (item, idx) {
+            if (wordToCheck == item) {
+              me.history.splice(idx, 1);
+            }
+          });
+
+        },
+
+        showSimpleToast: function (phrase) {
+          $mdToast.show(
+            $mdToast.simple()
+              .content(phrase)
+              .position(me.getToastPosition())
+              .hideDelay(2000)
+          );
+        },
+
+        getToastPosition: function () {
+          return Object.keys(toastPosition)
+            .filter(function (pos) {
+              return toastPosition[pos];
+            })
+            .join(' ');
+        },
+
+        toggleLeft: function () {
+          $mdSidenav('left-nav').toggle();
+        },
+
+        closeSideNav: function () {
+          $mdSidenav('left-nav').close();
+        },
+
+        deleteHistory: function () {
+          localStorage.removeItem('history');
+          me.history = [];
+        },
+
+        clearInput: function () {
+          me.setWord('');
+        },
+
+        setWord: function (word) {
+          me.wordInput = word;
+          setTimeout(function () {
+            _.each($scope.wordInputForm.word.$viewChangeListeners, _.attempt);
+          }, 100);
+        },
+
+        historyClicked: function (word) {
+          me.setWord(word);
+          me.kirciuoti(word);
+          me.closeSideNav();
+          $uiViewScroll($document.find('body'));
+        },
+
+        scrollTopClick: function () {
+          me.scrollTopElement[0].scrollTop = 0;
         }
 
-        $http.get('/api/krc/' + w).success(function(data) {
-          me.writeSearchedWords(w);
-          me.data = data;
-        }).error(function(data, res){
-          if(res == 404) {
-            me.data = [];
-            me.showSimpleToast('Žodis nerastas');
-          }
-          else if(res == 400){
-            me.showSimpleToast('Pasitikrintike įvestą žodį');
-          }
-          else if(res == 500){
-            me.showSimpleToast('Serverio klaida');
-          }
-        });
+      });
 
-      },
+      $scope.setSubNavs([{
+        name: 'history', title: 'Istorija', clickFn: me.toggleLeft, class: 'hide-gt-md'
+      }]);
 
-      writeSearchedWords: function(searchedWord){
-        me.duplicationRemover(searchedWord);
-        me.history.unshift(searchedWord);
-        me.history = _.take(me.history,20);
-      },
+      $scope.setLeftPart({
+        title: 'Istorija', class: 'hide-md hide-sm'
+      });
 
-      duplicationRemover: function(wordToCheck){
-        me.history.forEach(function(item,idx){
-          if (wordToCheck == item) {
-            me.history.splice(idx,1);
-          }
-        });
+    }
+  ])
 
-      },
+  .directive('resize', function ($window) {
+    return function (scope, element) {
+      var w = $window;
+      scope.getWindowDimensions = function () {
+        return {
+          'h': w.innerHeight,
+          'w': w.innerWidth
+        };
+      };
+      scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+        scope.windowHeight = newValue.h;
+        scope.windowWidth = newValue.w;
+        console.log(newValue.h, newValue.w);
+      }, true);
 
-      showSimpleToast: function(phrase){
-        $mdToast.show(
-          $mdToast.simple()
-            .content(phrase)
-            .position(me.getToastPosition())
-            .hideDelay(2000)
-        );
-      },
+      angular.element($window).bind('resize', function () {
+        scope.$apply();
+      });
+    }
+  })
 
-      getToastPosition: function() {
-        return Object.keys(toastPosition)
-          .filter(function(pos) { return toastPosition[pos]; })
-          .join(' ');
-      },
-
-      toggleLeft:function(){
-        $mdSidenav('left-nav').toggle();
-      },
-
-      closeSideNav: function() {
-        $mdSidenav('left-nav').close();
-      },
-
-      deleteHistory: function(){
-        localStorage.removeItem('history');
-        me.history = [];
-      },
-
-      clearInput: function () {
-        me.setWord('');
-      },
-
-      setWord: function (word) {
-        me.wordInput = word;
-        setTimeout(function(){
-          _.each($scope.wordInputForm.word.$viewChangeListeners, _.attempt);
-        },100);
-      },
-
-      historyClicked: function (word) {
-        me.setWord(word);
-        me.kirciuoti(word);
-        me.closeSideNav();
-        $uiViewScroll($document.find('body'));
-      },
-
-      scrollTopClick: function () {
-        me.scrollTopElement[0].scrollTop = 0;
-      }
-
-    });
-
-    $scope.setSubNavs([{
-      name: 'history', title: 'Istorija', clickFn: me.toggleLeft, class: 'hide-gt-md'
-    }]);
-
-    $scope.setLeftPart({
-      title: 'Istorija', class: 'hide-md hide-sm'
-    });
-
-  }
-]);
 
 
