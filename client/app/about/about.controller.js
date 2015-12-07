9740a02f4ca83fe7859368c4db2e13e2c920a2fd
TaWReOn <deniss.mosin@gmail.com>
2015-12-07 15:31:28 +0200

    controller added

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('stklcApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('AboutCtrl',['$document', '$scope', function ($document, $scope) {
+
+    var me = this;
+
+
+    angular.extend(me,{
+      buttonIsActive: false,
+      gitClicked: function(){
+        me.buttonIsActive = !me.buttonIsActive;
+        $document.find('a').unbind("mouseenter mouseleave");
+      }
+    });
+
+  }]);
