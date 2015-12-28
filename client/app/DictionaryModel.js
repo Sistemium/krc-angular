f5678d6d18d99cd37e389bb63a6d1ab636611c24
TaWReOn <deniss.mosin@gmail.com>
2015-12-28 17:03:21 +0200

    DictionaryModel service added

---
+++ client/app/DictionaryModel.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('stklcApp')
+  .service('DictionaryModel',['$http',function($http){
+
+    var me = this;
+
+    var formPlainDict  = function () {
+
+      var info;
+      var makePlainDict = function (obj){
+
+        _.forOwn(obj, function(val, key){
+
+          if (typeof(val) === 'object'){
+            info = key;
+            makePlainDict(val);
+          }
+
+          else{
+            me.dictPlain[key] = info+': '+val;
+          }
+
+        });
+
+      };
+
+      makePlainDict(me.strp);
+
+    };
+
+    console.log ('DictionaryModel init');
+
+    me.dictPlain = {};
+
+    return $http.get('/api/strp/').success(function (data) {
+      me.strp = data;
+      formPlainDict();
+    }).error(function (data, res) {
+      me.errorStatus = res;
+      console.log(res, 'Not found path');
+    }).promise;
+
+  }])
+;
