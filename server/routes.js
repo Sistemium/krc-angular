f51a30603a8e9e4bd0673d6304bcbe2fc3875be1
TaWReOn <deniss.mosin@gmail.com>
2016-01-12 18:26:37 +0200

    zodynas route added

--- server/routes.js
+++ server/routes.js
@@ -12,6 +12,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/krc', require('./api/krc'));
   app.use('/api/strp', require('./api/strp'));
+  app.use('/api/zodynas', require('./api/zodynas'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
