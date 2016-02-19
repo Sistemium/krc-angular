915ae2c3027480d43b76fa01acdfda5d9326fdf7
TaWReOn <deniss.mosin@gmail.com>
2016-02-19 13:58:59 +0200

    write to redis via waterline function

--- server/api/waterline/stat.controller.js
+++ server/api/waterline/stat.controller.js
@@ -31,6 +31,32 @@ module.exports.name = function (req, model, keys, data) {
 
 
 exports.index = function (req, res) {
+
+  var ua = UAParser(req.headers['user-agent']);
+
+  var currDate = moment().format('YYYY/MM/01');
+  var browserName = ua.browser.name;
+
+  var statsSaver = function (req, model, keys, data) {
+
+    req.app.models[model].findOne(keys)
+      .then (function (foundRec) {
+        console.log(foundRec);
+        if (!foundRec) {
+          return req.app.models[model]
+            .create (_.assignIn(keys, data))
+            .then (function (newRec) {
+              return newRec.save();
+            })
+            .catch (console.error);
+        } else {
+          foundRec.cnt++;
+          return foundRec.save()
+        }
+
+      });
+
+  };
 };
 
 
