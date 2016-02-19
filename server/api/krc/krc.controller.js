7f36e140be42565a13cc6982339df81ceebceab8
TaWReOn <deniss.mosin@gmail.com>
2016-02-19 13:53:57 +0200

    text formatting

--- server/api/krc/krc.controller.js
+++ server/api/krc/krc.controller.js
@@ -1,22 +1,27 @@
 'use strict';
 var request = require('request'),
-    fs = require('fs'),
-    cheerio = require('cheerio'),  /* HTML parser */
-    redisClient = require('../../config/redis').redisClient;
+  fs = require('fs'),
+  cheerio = require('cheerio'), /* HTML parser */
+  redisClient = require('../../config/redis').redisClient,
+  stats = require('../waterline/stat.controller'),
+  moment = require('moment');
 
 var link = 'http://donelaitis.vdu.lt/main.php?id=4&nr=9_1';
 // alternative http://www.zodynas.lt/kirciavimo-zodynas; form property == text
 
 var WORDS_HASH = 'kirtis_found_words';
 var NOT_FOUND_SET = 'kirtis_not_found_words';
-var debug = require('debug') ('krc:controller');
+var debug = require('debug')('krc:controller');
+
+
 
 exports.index = function (req, res) {
+
 
   debug('Word typed:', req.params.word);
   var text = req.params.word; // Viena for testing
   text = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase(); // uppercase first letter lowercase other
-  var failmsg = 'You\'ve got '+ false +' value. Please check the spelling of the word "' + text +'"';
+  var failmsg = 'You\'ve got ' + false + ' value. Please check the spelling of the word "' + text + '"';
 
   redisClient.HGET(WORDS_HASH, text, function (err, response) {
 
@@ -24,7 +29,7 @@ exports.index = function (req, res) {
       console.log(err);
     }
 
-    if(!response) {
+    if (!response) {
       redisClient.SISMEMBER(NOT_FOUND_SET, text, function (err, r) {
         if (err) {
           console.log(err);
@@ -67,12 +72,15 @@ function sendRequest(res, text) {
       return res.status(500).send('The server is down. Please try later.');
     }
 
-    var stressArray = stressedWord.split('\r\n'); /* turning string to a string array */
-    stressArray.splice(-1, 1); /* deleting the last array elem. which is empty string */
+    var stressArray = stressedWord.split('\r\n');
+    /* turning string to a string array */
+    stressArray.splice(-1, 1);
+    /* deleting the last array elem. which is empty string */
     var regexp = /^[^ ]+[ ]([^ ]+) \(([^)]+)/;
     var regexpNoState = /[^\d.\s][^()]/g;
     var wordApi = [];  // stressed word state and etc
-    var arrLen = stressArray.length;  /* Count of stressed word found. ex. Viena 15*/
+    var arrLen = stressArray.length;
+    /* Count of stressed word found. ex. Viena 15*/
 
     function formWordStructure(stressArray) {
       stressArray.forEach(function (item) {
@@ -90,7 +98,8 @@ function sendRequest(res, text) {
         else {
           var jsonObj = {};
           jsonObj.word = stressArray.join(' ').match(regexpNoState).join('');
-          jsonObj.class = 'dktv.'; /* Not sure, but fits well */
+          jsonObj.class = 'dktv.';
+          /* Not sure, but fits well */
           wordApi.push(jsonObj);
         }
 
@@ -105,13 +114,13 @@ function sendRequest(res, text) {
 
       if (wordApi.length) {
 
-        redisClient.HSET(WORDS_HASH, text, JSON.stringify(wordApi),function (err, r) {
+        redisClient.HSET(WORDS_HASH, text, JSON.stringify(wordApi), function (err, r) {
 
           if (r === 1) {
             debug('Word found. \nWord', text, 'added to', WORDS_HASH, 'database', 'on', Date());
           }
 
-          else{
+          else {
             console.log('Error occurred while writing to a database', WORDS_HASH);
           }
 
@@ -123,7 +132,7 @@ function sendRequest(res, text) {
     }
     else {
 
-      redisClient.SADD(NOT_FOUND_SET, text,function (err, r) {
+      redisClient.SADD(NOT_FOUND_SET, text, function (err, r) {
 
         if (r === 1) {
           debug('Word not found.\nWord', text, 'added to', NOT_FOUND_SET, 'database on', Date());
@@ -133,7 +142,7 @@ function sendRequest(res, text) {
           console.log('Error occurred while writing to a database', NOT_FOUND_SET);
         }
 
-        var failmsg = 'You\'ve got '+ false +' value. Please check the spelling of the word "' + text +'"';
+        var failmsg = 'You\'ve got ' + false + ' value. Please check the spelling of the word "' + text + '"';
 
         res.status(404).send(failmsg);
 
