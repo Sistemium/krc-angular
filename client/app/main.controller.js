a8435586afd0b294b0e70f140fc03a10fc1d41a8
TaWReOn <deniss.mosin@gmail.com>
2016-02-19 14:03:00 +0200

    code formatting + get request to write user stats ( browserInfo, userCount)

--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -1,38 +1,40 @@
 'use strict';
 
 angular.module('stklcApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     var me = this;
 
-    angular.extend(me,{
+    angular.extend(me, {
 
       navs: [
-        { name: 'krc', title: 'Kirčiavimo programa', sref: 'krc', icon: 'action:exit_to_app'},
-        { name: 'about', title: 'Apie projektą', sref: 'about', icon: 'action:question_answer'}
+        {name: 'krc', title: 'Kirčiavimo programa', sref: 'krc', icon: 'action:exit_to_app'},
+        {name: 'about', title: 'Apie projektą', sref: 'about', icon: 'action:question_answer'}
       ],
 
-      openMenu: function($mdOpenMenu, ev) {
+      openMenu: function ($mdOpenMenu, ev) {
         $mdOpenMenu(ev);
       }
 
     });
 
-    $scope.setSubNavs = function(navs) {
+    $scope.setSubNavs = function (navs) {
       me.subNavs = navs || [];
     };
 
-    $scope.setLeftPart = function(part) {
-     me.leftPart = part;
+    $scope.setLeftPart = function (part) {
+      me.leftPart = part;
     };
 
-    $scope.$on('$stateChangeSuccess', function() {
+    $scope.$on('$stateChangeSuccess', function () {
       $scope.setSubNavs();
       $scope.setLeftPart();
     });
 
-    $scope.disableScroll = function(arg) {
+    $scope.disableScroll = function (arg) {
       $scope.scrollDisabled = !!arg;
     };
+
+    $http.get('/api/stats');
 
   }]);
