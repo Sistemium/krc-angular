0dcdb83e1329d93468a805b17bd5ffcdbf861139
TaWReOn <deniss.mosin@gmail.com>
2016-03-22 15:14:24 +0200

    stapi url

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -8,5 +8,6 @@ module.exports = {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
     db: process.env.REDIS_DATABASE || 1
-  }
+  },
+  STAPI: 'https://api.sistemium.com/v4d/krc/'
 };
