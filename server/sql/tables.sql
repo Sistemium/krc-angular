874f1ba0d627f1f398eace87a982ff2f9ed8ef8b
Alexander Levin <alevin.ru@gmail.com>
2016-03-24 14:21:40 +0200

    deviceUUID sql

--- server/sql/tables.sql
+++ server/sql/tables.sql
@@ -2,9 +2,10 @@ grant connect to krc;
 grant dba to krc;
 
 meta.defineType 'krc.word:STRING';
+meta.defineType 'krc.deviceUUID:uniqueidentifier,,nullable';
 
-meta.defineEntity 'krc.NotFoundWord', 'word';
-meta.defineEntity 'krc.FoundWord', 'word';
+meta.defineEntity 'krc.NotFoundWord', 'word;deviceUUID';
+meta.defineEntity 'krc.FoundWord', 'word;deviceUUID';
 
 meta.createTable 'krc.NotFoundWord',0,1;
 meta.createTable 'krc.FoundWord',0,1;
