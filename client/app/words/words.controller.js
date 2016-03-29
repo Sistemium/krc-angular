fc7e2d77bd6879e25a32db38e69ed699cbb2258c
TaWReOn <deniss.mosin@gmail.com>
2016-03-29 17:24:21 +0300

    words page controller added

--- client/app/words/words.controller.js
+++ client/app/words/words.controller.js
@@ -1,72 +1,137 @@
 'use strict';
 angular.module('stklcApp')
   .controller('WordsCtrl', ['$timeout', '$http', function ($timeout, $http) {
-    //
-    //this.infiniteItems = {
-    //  numLoaded_: 0,
-    //  toLoad_: 0,
-    //
-    //  // Required.
-    //  getItemAtIndex: function(index) {
-    //    console.log('Are you first??, get item at index');
-    //    if (index > this.numLoaded_) {
-    //      this.fetchMoreItems_(index);
-    //      return null;
-    //    }
-    //    console.log(index, 'index');
-    //    console.log(this.numLoaded_, 'numLoaded');
-    //
-    //    return index;
-    //  },
-    //
-    //  // Required.
-    //  // For infinite scroll behavior, we always return a slightly higher
-    //  // number than the previously loaded items.
-    //  getLength: function() {
-    //    console.log('Are you first??, getlength');
-    //    return this.numLoaded_ + 5;
-    //  },
-    //
-    //  fetchMoreItems_: function(index) {
-    //
-    //    console.log('Are you first??, fetchmoreitems');
-    //    // For demo purposes, we simulate loading more items with a timed
-    //    // promise. In real code, this function would likely contain an
-    //    // $http request.
-    //    //console.log(index, 'index');
-    //    //console.log(this.toLoad_, 'toLoad');
-    //
-    //    if (this.toLoad_ < index) {
-    //      this.toLoad_ += 20;
-    //      $timeout(angular.noop, 3000).then(angular.bind(this, function() {
-    //        this.numLoaded_ = this.toLoad_;
-    //      }));
-    //    }
-    //  }
-    //};
+    // In this example, we set up our model using a plain object.
+    // Using a class works too. All that matters is that we implement
+    // getItemAtIndex and getLength.
 
-    //$http({
-    //  method: 'GET',
-    //  url: 'https://api.sistemium.com/v4d/krc/notFoundWord?agg:=1'
-    //}).then(function successCallback(response) {
-    //  var test = (response.headers());
-    //  console.log(test['x-aggregate-count']);
-    //
-    //}, function errorCallback(response) {
-    //  console.log(response);
-    //});
-    //
-    //
-    //$http({
-    //  method: 'GET',
-    //  url: 'https://api.sistemium.com/v4d/krc/notFoundWord?x-page-size:=200'
-    //}).then(function successCallback(response) {
-    //    var notFound =  response.data;
-    //    console.log(notFound);
-    //}, function errorCallback(response) {
-    //  console.log(response);
-    //});
+    // In this example, we set up our model using a class.
+    // Using a plain object works too. All that matters
+    // is that we implement getItemAtIndex and getLength.
+    var NotFoundWords = function () {
+      /**
+       * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
+       */
+      this.loadedPages = {};
+
+      /** @type {number} Total number of items. */
+      this.numItems = 0;
+
+      /** @const {number} Number of items to fetch per request. */
+      this.PAGE_SIZE = 10;
+
+      this.fetchNumItems_();
+    };
+
+    // Required.
+    NotFoundWords.prototype.getItemAtIndex = function (index) {
+      var pageNumber = Math.floor(index / this.PAGE_SIZE);
+      var page = this.loadedPages[pageNumber];
+
+      if (page) {
+        return page[index % this.PAGE_SIZE];
+      } else if (page !== null) {
+        this.fetchPage_(pageNumber);
+      }
+    };
+
+    // Required.
+    NotFoundWords.prototype.getLength = function () {
+      return this.numItems;
+    };
+
+    NotFoundWords.prototype.fetchPage_ = function (pageNumber) {
+      // Set the page to null so we know it is already being fetched.
+      this.loadedPages[pageNumber] = null;
+
+      // For demo purposes, we simulate loading more items with a timed
+      // promise. In real code, this function would likely contain an
+      // $http request.
+
+      $http.get('https://api.sistemium.com/v4d/krc/notFoundWord?x-start-page:=' + (pageNumber + 1)).then(angular.bind(this, function (obj) {
+        this.loadedPages[pageNumber] = [];
+
+        for (var i in obj.data) {
+          this.loadedPages[pageNumber].push(obj.data[i].word);
+        }
+      }));
+    };
+
+    NotFoundWords.prototype.fetchNumItems_ = function () {
+
+      if (this.numItems == 0) {
+
+        $http.get('https://api.sistemium.com/v4d/krc/notFoundWord?agg:=1').then(angular.bind(this, function (obj) {
+          this.numItems = obj.headers('X-Aggregate-Count');
+        }));
+      }
+
+    };
 
 
-  }]);
+    this.notFoundWords = new NotFoundWords();
+
+
+    var FoundWords = function () {
+      /**
+       * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
+       */
+      this.loadedPages = {};
+
+      /** @type {number} Total number of items. */
+      this.numItems = 0;
+
+      /** @const {number} Number of items to fetch per request. */
+      this.PAGE_SIZE = 10;
+
+      this.fetchNumItems_();
+    };
+
+    // Required.
+    FoundWords.prototype.getItemAtIndex = function (index) {
+
+      var pageNumber = Math.floor(index / this.PAGE_SIZE);
+      var page = this.loadedPages[pageNumber];
+
+      if (page) {
+        return page[index % this.PAGE_SIZE];
+      } else if (page !== null) {
+        this.fetchPage_(pageNumber);
+      }
+    };
+
+    // Required.
+    FoundWords.prototype.getLength = function () {
+      return this.numItems;
+    };
+
+    FoundWords.prototype.fetchPage_ = function (pageNumber) {
+      // Set the page to null so we know it is already being fetched.
+      this.loadedPages[pageNumber] = null;
+
+      $http.get('https://api.sistemium.com/v4d/krc/foundWord?x-start-page:=' + (pageNumber + 1)).then(angular.bind(this, function (obj) {
+        this.loadedPages[pageNumber] = [];
+        for (var i in obj.data) {
+          this.loadedPages[pageNumber].push(obj.data[i].word);
+        }
+      }));
+    };
+
+    // Getting total number of elements to show in viewport
+
+    FoundWords.prototype.fetchNumItems_ = function () {
+      if (this.numItems == 0) {
+        $http.get('https://api.sistemium.com/v4d/krc/foundWord?agg:=1').then(angular.bind(this, function (obj) {
+          this.numItems = obj.headers('X-Aggregate-Count');
+        }));
+      }
+
+    };
+
+    this.foundWords = new FoundWords();
+
+
+  }
+  ]);
+
 
